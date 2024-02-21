bfbff3835dc8efe9d0c77bd135e2af25d9dc5631
김초향 <chkim9910@naver.com>
2024-02-21 17:36:17 +0900

    resolve horizontal problem

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
+  gsap.registerPlugin(ScrollTrigger);
   // 페이지가 로드될 때 스크롤을 맨 위로 이동
   window.onload = function () {
     window.scrollTo(0, 0);
     $("body").css("overflow", "hidden");
   };
-  gsap.set($(".scrollBox .wrap > div").not("#intro"), { yPercent: 100 });
   // intro
   $("#intro .btn-icon").on("click", function () {
     var a = document.getElementById("intro_video_inner");
@@ -13,10 +13,6 @@ $(document).ready(function () {
     var tl = gsap.timeline();
     tl.to([$("#intro")], { height: 0, duration: 1 }, 2)
       .to($("#header"), { top: "0px" })
-      .to($(".scrollBox .wrap > div").not("#intro"), {
-        yPercent: 0,
-        duration: 0.6,
-      })
       .to($("body"), { overflow: "visible" });
     window.scrollTo(0, 0);
   });
@@ -28,8 +24,6 @@ $(document).ready(function () {
     sect02_total += $(".fix-box-2 .page").eq(i).width();
   }
 
-  gsap.registerPlugin(ScrollTrigger);
-
   gsap.to(".fix-box-2", {
     x: -(sect02_total - innerWidth),
     scrollTrigger: {
@@ -39,28 +33,39 @@ $(document).ready(function () {
       pin: true,
       pinSpacing: false,
       scrub: true,
-      markers: true, // ScrollTrigger 디버깅용 마커 표시
-    },
-    // onPin: () => {
-    //   console.log(".fix-box-2가 pin 되었습니다.");
-    // },
-    // onUnpin: () => {
-    //   console.log(".fix-box-2가 pin 되지 않았습니다.");
-    // },
-    onStart: () => {
-      console.log("sect02에 들어왔습니다.");
     },
   });
 
-  gsap.to("#scene02 .page01", {
-    scrollTrigger: {
-      trigger: "#scene02",
-      start: "-600 top",
-      end: "bottom bottom",
-      scrub: true,
-      toggleClass: "on",
-    },
-  });
+  // gsap.to(".fix-box-2", {
+  //   x: -(sect02_total - innerWidth),
+  //   scrollTrigger: {
+  //     trigger: ".trigger-box-2",
+  //     start: "top top",
+  //     end: "bottom bottom",
+  //     pin: true,
+  //     pinSpacing: false,
+  //     scrub: true,
+  //   },
+  //   // onPin: () => {
+  //   //   console.log(".fix-box-2가 pin 되었습니다.");
+  //   // },
+  //   // onUnpin: () => {
+  //   //   console.log(".fix-box-2가 pin 되지 않았습니다.");
+  //   // },
+  //   // onStart: () => {
+  //   //   console.log("sect02에 들어왔습니다.");
+  //   // },
+  // });
+
+  // gsap.to("#scene02 .page01", {
+  //   scrollTrigger: {
+  //     trigger: "#scene02",
+  //     start: "-600 top",
+  //     end: "bottom bottom",
+  //     scrub: true,
+  //     toggleClass: "on",
+  //   },
+  // });
 
   // gsap.to("#scene02 .page02", {
   //   scrollTrigger: {
